138c977c90fd46f973263ad45b285d3636c2586c
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-08 15:17:59 -0800

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Main entry point
  * This project uses putchar
@@ -7,6 +8,6 @@
 
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
