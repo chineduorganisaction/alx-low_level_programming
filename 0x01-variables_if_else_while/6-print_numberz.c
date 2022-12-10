2ded9e8ae7c985e1551aaa229b4bce06111aa3dc
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-10 00:52:22 -0800

    c variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Main is entry point
+ *
+ * Return: prints an integer at code end
+*/
+
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		putchar('-');
+		nmuber++;
+	}
+	putchar('\n');
+	return (0);
+}
