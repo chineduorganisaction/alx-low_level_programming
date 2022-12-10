235d3a4816e337a1f06c76543b680142cc972617
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-10 01:06:53 -0800

    c variables

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Main is entry point
+ *
+ * Return: print integer at code end
+*/
+
+int main(void)
+{
+	char alph = 'z';
+
+	while('a' < alph)
+	{
+		putchar(alph);
+		--alp;
+	}
+	putchar('\n');
+	return (0);
+}
