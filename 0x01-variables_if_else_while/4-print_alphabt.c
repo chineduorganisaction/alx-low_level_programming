a31af6fd5ab0b3dfc8efbc24af53b0dc7feb5226
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-10 00:01:21 -0800

    c variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Main is entry point
+ *
+ * Return: print integer at code end
+*/
+
+int main(void)
+{
+	char lower = 'a';
+
+	while (lower = 'z')
+	{
+		lower++;
+		if (lower != 'e' && lower != 'q')
+		{
+			putchar(lower);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
