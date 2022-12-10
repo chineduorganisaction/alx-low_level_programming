788510a5ee7027ad2e5ecf99f68c31e163f59bdc
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-09 22:54:54 -0800

    c variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Main is entry point
+ *
+ * Return: print integer to end program
+*/
+
+int main(void)
+{
+	char lower = "D";
+	putchar("Transform %c to lowercase like this %c\n", lower, tolower(lower));
+	return (0);
+}
