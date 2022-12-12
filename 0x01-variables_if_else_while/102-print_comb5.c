0a2eecdbf1ec5b921442c74a6d98a06c8c366c56
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-11 21:07:41 -0800

    fixes

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Main is entry point
+ *
+ * Return: print an integer at code end
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 00; n < 99; n++)
+	{
+		for (m = n + 1; m < 100; m++)
+		{
+			putchar((n % 100) + '0');
+			putchar(' ');
+			putchar((m % 100) + '0');
+			if (n == 98 && m == 99)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
