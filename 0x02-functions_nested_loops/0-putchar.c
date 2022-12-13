1a6b3e70b0b6893192918c1ab4ecca349d8e3ad3
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-13 12:14:47 -0800

    function loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Main is the entry point
+ *
+ * Return: print an integer at code end
+ */
+
+int main(void)
+{
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	putchar("\n");
+	return (0);
+}
