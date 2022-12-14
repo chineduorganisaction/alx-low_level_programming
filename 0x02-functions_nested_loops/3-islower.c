46c60ac6e4dd7a6b12eda20a201fe5c7f76238f0
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-14 04:13:47 -0800

    fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -14,6 +14,7 @@ int main(void)
 
 	r = _islower('H');
 	_putchar(r + '0');
+
 	_putchar('\n');
 	return (0);
 }
@@ -21,16 +22,16 @@ int main(void)
 int _islower(int c)
 {
 	char letter;
-
+	
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (c == letter)
 		{
-			return (1);
+			return 1;
 		}
 		else
 		{
-			return (0);
+			return 0;
 		}
 	}
 }
