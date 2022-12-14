be9c708cb6e4b33c2443522b33b9ce5767ed9ed5
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-14 14:10:52 -0800

    fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,11 +3,13 @@
  * _isalpha - entry point for alphabet check
  *
  * @c: is a peremeter for variables
+ *
+ * Return: print an integer at code end
  **/
 
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 &&c <= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
