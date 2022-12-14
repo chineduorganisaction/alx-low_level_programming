b9d0f9e9f862c4d796692054a262784773f6d3dc
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-14 15:04:11 -0800

    added codes

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit: main entry point
+ *
+ * @n: parameter
+ *
+ * Return: print integer answer
+ **/
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
