32a34214ea665dd6c4ab7403eb5b064f9394eb90
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-15 05:30:14 -0800

    multiple of number

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,19 +7,17 @@
 
 int multiple(void)
 {
-	int n, sum3, sum5;
+	int n, sum3 = 0, sum5 = 0;
 
 	for (n = 0; n < 100; n++)
 	{
 		if ((n % 3) == 0)
 		{
-			sum3 = 0;
 			sum3 = sum3 + n;
 			printf("%d", sum3);
 		}
 		if ((n % 5) == 0)
 		{
-			sum5 = 0;
 			sum5 = sum5 + n;
 			printf("%d", sum5);
 		}
