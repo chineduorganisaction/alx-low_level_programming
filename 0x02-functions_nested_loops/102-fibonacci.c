85cb9c6f9d47588a9e71a3fbcb29111a347f0dce
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-15 11:13:53 -0800

    fix

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ *
+ * Return: print integer
+ **/
+
+int main(void)
+{
+	int n1 = 0, n2 = 1, nextvar = 0, i;
+
+	nextvar = n1 + n2;
+	printf("%d", nextvar);
+	for (i = 1; i < 50; i++)
+	{
+		printf("%d, ", nextvar);
+		n1 = n2;
+		n2 = nextvar;
+	}
+	return (0);
+}	
