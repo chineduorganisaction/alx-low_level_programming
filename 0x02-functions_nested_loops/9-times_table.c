dfbd6f6a047500ff12852e75580afe85583b5c38
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-15 00:19:43 -0800

    add new code

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,14 +9,22 @@ void times_table(void)
 {
 	int m, n, d;
 
+	_putchar('0');
+	_putchar(',');
+	_putchar(' ');
+
 	for (m = 0; m < 10; m++)
 	{
-		for (n = 0; n < 10; n++)
+		for (n = 1; n < 10; n++)
 		{
 			d = (n * m);
 			if ((d / 10) > 0)
 			{
 				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
 			}
 			_putchar((d % 10) + '0');
 
