136c4b3e3e0f32d3e90f202c5fdd042354188265
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-15 00:52:20 -0800

    add new code

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,6 +13,11 @@ void print_to_98(int n)
 		while (n > 98)
 		{
 			_putchar('0' + n);
+			if (n > 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 			--n;
 		}
 	}
@@ -21,6 +26,11 @@ void print_to_98(int n)
 		while (n < 98)
 		{
 			_putchar('0' + n);
+			if (n < 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 			++n;
 		}
 	}
