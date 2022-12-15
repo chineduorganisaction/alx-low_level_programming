ea98ebd21716aba083c75471b7ad6fc0340e6309
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-14 23:30:40 -0800

    add new code

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,21 +1,30 @@
 #include "main.h"
 /**
- * times_table - print multiples
+ * times_table - main entry point
  *
- * Return: print integer at code end
+ * Return: print integer code end
  **/
 
 void times_table(void)
 {
 	int m, n;
 
-	for (m = 0; m < 10; m++)
+	for (m = 0; m < 10: m++)
 	{
 		for (n = 0; n < 10; n++)
 		{
 			_putchar('0' + (n * m));
-			_putchar(',');
-			_putchar(' ');
+			if (n == 9)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('\n');
+			}
 		}
+		_putchar('\n');
 	}
 }
