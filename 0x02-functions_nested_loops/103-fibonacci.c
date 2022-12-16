b93db1b753d0670be221d3a9687933befbf3c216
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-16 01:01:35 -0800

    fix

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main entry point                   * Return: print integer                     **/
+
+int main(void)
+{
+        long int n1 = 0, n2 = 1, nextvar = 0, i, sum2 = 0;
+
+        nextvar = n1 + n2;                          printf("%ld, ", nextvar);
+        for (i = 1; i < 50; i++)
+        {
+                if (nextvar < 4000000)
+		{
+			n1 = nextvar;
+                	nextvar = (n2 + n1);
+                	n2 = n1;
+			if ((nextvar % 2) == 0)
+			{
+				sum2 = (sum2 + nextvar);
+				printf("%ld", sum2);
+			}
+		}
+        }
+        printf("\n");
+        return (0);
+}
