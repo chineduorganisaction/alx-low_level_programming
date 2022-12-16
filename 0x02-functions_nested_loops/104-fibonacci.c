10a40f0285b62fd7470b8e625b1f6962a0e9592c
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-16 04:29:23 -0800

    fix

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ * Return: prints integer
+ **/
+
+int main(void)
+{
+	long int n1 = 0, n2 = 1, m98 = 0, i;
+
+	m98 = (n1 + n2);
+	for (i = 1; i <= 98; i++)
+	{
+		n1 = m98;
+		m98 = (n2 + n1);
+		printf("%ld", m98);
+		n2 = n1;
+		if (i != 98)
+		{
+			printf(", ");
+		}
+	}
+	print("\n");
+	return (0);
+}
