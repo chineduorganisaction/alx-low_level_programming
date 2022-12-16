96d318b3a137432e0ebe635b00e90e16584b95e4
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-16 05:11:57 -0800

    fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,15 +6,15 @@
 
 int main(void)
 {
-	long int n1 = 0, n2 = 1, m98 = 0, i;
+	unsigned int n1 = 0, n2 = 1, m98 = 0, i;
 
 	m98 = (n1 + n2);
-	printf("%ld, ", m98);
-	for (i = 1; i < 98; i++)
+	printf("%u, ", m98);
+	for (i = 1; i <= 98; i++)
 	{
 		n1 = m98;
 		m98 = (n2 + n1);
-		printf("%ld", m98);
+		printf("%u", m98);
 		n2 = n1;
 		if (i != 98)
 		{
