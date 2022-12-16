e45cac113b1f8952095ff3f190363dbd14152256
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-16 02:13:40 -0800

    fix

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,21 +6,22 @@
 
 int main(void)
 {
-	long int sum2 = 0, nextvar = 0;
+	long int sum2 = 0, nextvar = 0, sum = 0;
 
 	int n1 = 0, n2 = 1;
 
 	while (nextvar < 4000000)
 	{
 		n1 = nextvar;
-		nextvar = (n1 + n2);
 		n2 = n1;
 		if ((nextvar % 2) == 0)
 		{
-			sum2 = (sum2 + nextvar);
-			printf("%ld", sum2);
+			sum2 = nextvar;
 		}
+		nextvar = (n1 + n2);
 	}
+	sum = sum + sum2;
+	printf("%ld", sum);
 	printf("\n");
 	return(0);
 }
