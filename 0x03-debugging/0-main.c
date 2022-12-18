b93313d7b398c3bcc36aa08d44e951b120d2975b
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-17 22:09:36 -0800

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - main entry point
+ * @i: parameter
+ * Return: prints integer
+ **/
+
+int main(int i)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
