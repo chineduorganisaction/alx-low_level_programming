b78b081e9afa179005e3eb8213c084770870a5a6
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-18 01:07:14 -0800

    new

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,19 +12,20 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
+	largest = 0;
+
 	if (a > b && b > c)
 	{
 		largest = a;
-		return (largest);
 	}
 	else if (b > a && a > c)
 	{
 		largest = b;
-		return (largest);
 	}
 	else
 	{
 		largest = c;
-		return (largest);
 	}
+
+	return (largest);
 }
