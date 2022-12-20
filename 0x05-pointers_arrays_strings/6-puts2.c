ab61cd2793a47324c5b1bd7783e1b1a05ac62ce5
chineduorganisaction <chineduorganisaction@gmail.com>
2022-12-20 03:45:35 -0800

    pointers arrays

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,13 @@
 
 void puts2(char *str)
 {
-	int i, j;
+	int i, j = 0;
 
 	while (str[j] != '\0')
 	{
 		j++;
 	}
-	for (i = 0; i < j; i++)
+	for (i = 0; i < j; i += 2)
 	{
 		_putchar(str[i]);
 	}
